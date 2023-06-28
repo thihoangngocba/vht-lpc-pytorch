76490b790cb26c404836fd7db935a1f0c3d50903
HOÀNG NGỌC BÁ THI <115174809+thihoangngocba@users.noreply.github.com>
2023-06-28 10:32:45 +0700

    Update train.py

--- train.py
+++ train.py
@@ -116,8 +116,9 @@ def main():
         train(train_loader, model, loss_fn, optimizer)
         val(val_loader, model, loss_fn)
 
-    if os.path.exists(args.output_path) == False:
-        os.makedirs(args.output_path)
+    output_dir = (args.output_path).split("/")[:-1]
+    if os.path.exists(output_dir) == False:
+        os.makedirs(output_dir)
     torch.save(model, args.output_path)
     print("DONE! SAVED MODEL TO {}", args.output_path)
 
